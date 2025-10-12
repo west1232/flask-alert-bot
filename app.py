0448066f5373bbd99e7dc6576ef23aba949301b4
west1232 <92494478+west1232@users.noreply.github.com>
2025-10-12 12:28:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Discord通知（MacroDroid経由）のテキストを受け取る
-    text = request.data.decode('utf-8')  # ← JSONではなくプレーンテキストで受信
+    text = request.data.decode('utf-8')
     print(f"通知受信: {text}")
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
