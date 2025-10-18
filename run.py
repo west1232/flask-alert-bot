79c2101daa353c72b34295bec513fdaece1c7e36
west1232 <92494478+west1232@users.noreply.github.com>
2025-10-18 22:34:34 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
+# Flask アプリ生成
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    # 開発用サーバー起動（Render 上では gunicorn が使用）
+    app.run(host="0.0.0.0", port=5000, debug=True)
